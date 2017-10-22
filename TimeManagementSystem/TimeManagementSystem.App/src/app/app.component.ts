edec86a45bb7a9133ec2d2d2b84302e31c3bac6a
Mateusz Rogulski <mateusz@rogulscy.pl>
2017-10-22 22:36:13 +0200

    App in ng4 1.0

---
+++ TimeManagementSystem/TimeManagementSystem.App/src/app/app.component.ts
@@ -0,0 +1,29 @@
+﻿import { Component, ViewContainerRef } from '@angular/core';
+import { ToastsManager } from 'ng2-toastr/ng2-toastr';
+import { Router } from '@angular/router';
+import { PermissionLevel } from './shared/enum';
+
+@Component({
+  selector: 'tms-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+    constructor(public toastr: ToastsManager, vRef: ViewContainerRef, private _router: Router) {
+        this.toastr.setRootViewContainerRef(vRef);
+    }
+
+    role: PermissionLevel = +localStorage.getItem("role");
+
+    isAnonymous: boolean = this.role === null;
+    isAdministrator: boolean = this.role === PermissionLevel.administrator;
+    isUserManager: boolean = this.role === PermissionLevel.userManager;
+    isRegular: boolean = this.role === PermissionLevel.regular;
+    user = localStorage.getItem("user");
+
+    logOut(): void {
+        localStorage.removeItem("token");
+        localStorage.removeItem("role");
+        this._router.navigate(['login']);
+    }
+}
---
+++ TimeManagementSystem/TimeManagementSystem.App/src/app/app.module.ts
@@ -0,0 +1,36 @@
+﻿import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ToastModule } from 'ng2-toastr/ng2-toastr';
+import { HttpModule } from '@angular/http';
+import { HttpClientModule } from "@angular/common/http";
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DataService } from './services/data.service';
+import { LoginComponent } from './components/login/login.component';
+import { UsersComponent } from './components/users/users.component';
+import { UserComponent } from './components/users/user/user.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    UsersComponent,
+    UserComponent
+  ],
+  imports: [
+    BrowserAnimationsModule,
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    HttpModule,
+    ToastModule.forRoot(),
+    HttpClientModule
+  ],
+  providers: [DataService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
